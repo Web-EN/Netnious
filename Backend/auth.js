73839b115e643e87c300a199cca410703f33e8d0
Alvaro Chavez <alvaro.chavez6@unmsm.edu.pe>
2023-10-22 20:18:47 -0500

    Help

---
+++ Backend/auth.js
@@ -0,0 +1,102 @@
+const jwt = require('jsonwebtoken');
+const crypto = require('crypto');
+// No es necesario crear una instancia de cliente aquí
+
+// Para consulta
+async function getUserFromDatabase(username, password, client) {
+  try {
+    // Realizar una consulta SQL para verificar las credenciales
+    const query = {
+      text: 'SELECT usuario, hashed_pass FROM public.usuarios WHERE usuario = $1 AND hashed_pass = $2',
+      values: [username, password],
+    };
+
+    const result = await client.query(query);
+
+    if (result.rows.length === 1) {
+      const user = result.rows[0];
+      return {
+        id: user.id,
+        username: user.username,
+      };
+    } else {
+      return null;
+    }
+  } catch (error) {
+    console.error('Error al consultar la base de datos:', error);
+    throw error;
+  }
+}
+
+// Para generar token
+function generateAuthToken(user) {
+  const secretKey = crypto.randomBytes(32).toString('hex');
+
+  // Solo define el token
+  const payload = {
+    userId: user.id,
+    username: user.username,
+    // Se pueden incluir más datos
+  };
+
+  // Genera el token JWT con la información y la clave secreta
+  const token = jwt.sign(payload, secretKey, { expiresIn: '1h' });
+
+  return token;
+}
+
+module.exports = { getUserFromDatabase, generateAuthToken };
+
+/*
+Para hacerlo de una en caso ya no te salga el Call Stack:
+
+const jwt = require('jsonwebtoken');
+const crypto = require('crypto');
+const { Client } = require('pg')
+
+//Para consulta
+async function getUserFromDatabase(username, password, client) {
+  try {
+    await client.connect();
+    const query = {
+      text: 'SELECT usuario, hashed_pass FROM public.usuarios WHERE usuario = $1 AND hashed_pass = $2',
+      values: [username, password],
+    };
+
+    const result = await client.query(query);
+
+    if (result.rows.length === 1) {
+      const user = result.rows[0];
+      return {
+        id: user.id,
+        username: user.username,
+      };
+    } else {
+      return null;
+    }
+  } catch (error) {
+    console.error('Error al consultar la base de datos:', error);
+    throw error;
+  } finally {
+    await client.end();
+  }
+}
+//Para generar token
+function generateAuthToken(user) {
+    
+  const secretKey = crypto.randomBytes(32).toString('hex');
+
+  //Solo define el token
+  const payload = {
+    userId: user.id,
+    username: user.username,
+    //Se pueden incluir más datos
+  };
+
+  //Genera el token JWT con la información y la clave secreta
+  const token = jwt.sign(payload, secretKey, { expiresIn: '1h' });
+
+  return token;
+}
+
+module.exports = { getUserFromDatabase, generateAuthToken };*/
--- Login/lib/main.dart
+++ Login/lib/main.dart
@@ -9,19 +9,19 @@ void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
-	Widget build(BuildContext context) {
-	return MaterialApp(
-		title: 'Flutter',
-		debugShowCheckedModeBanner: false,
-		scrollBehavior: MyCustomScrollBehavior(),
-		theme: ThemeData(
-		primarySwatch: Colors.blue,
-		),
-		home: Scaffold(
-		body: SingleChildScrollView(
-			child: Scene(),
-		),
-		),
-	);
-	}
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter',
+      debugShowCheckedModeBanner: false,
+      scrollBehavior: MyCustomScrollBehavior(),
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        body: SingleChildScrollView(
+          child: Scene(),
+        ),
+      ),
+    );
+  }
 }
